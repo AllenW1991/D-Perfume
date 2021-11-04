d15a5a48d5b4292bcb62093b6be128ee59b88285
AllenW1991 <venecia631201@yahoo.com.tw>
2021-11-04 15:30:48 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+$(function () {
+
+
+  $("#addNumber").on("click", function () {
+    let number = parseInt($("#product-number").val());
+    number += 1;
+    $("#product-number").val(number);
+  })
+
+
+  $("#reduceNumber").on("click", function () {
+    let number = parseInt($("#product-number").val());
+    number -= 1;
+    $("#product-number").val(number);
+    if (number < 1) {
+      number = 0;
+      $("#product-number").val(number);
+    }
+  })
+})
